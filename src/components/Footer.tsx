1461c1fa71baa74f911c2461546040a3005089df
Raymond Salim <rsalim@domaintools.com>
2026-07-16 10:41:51 +0700

    fix(footer): update copyright year and fix stale open-source repo link
    The link still pointed at the old PersonalWebsite repo name, 404ing
    since the repo was renamed to raymonds.dev.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,11 +11,11 @@ export class Footer extends React.Component<any, any> {
               className={'h-12 w-12'}
             />
             <span className={'my-2'}>Thanks for visiting!</span>
-            <span className={'mt-6'}>© Copyright 2022, Raymond Salim</span>
+            <span className={'mt-6'}>© Copyright 2026, Raymond Salim</span>
           </div>
           <div>
             <div id="sellout">
-              <a href="https://github.com/RaymondSalim/PersonalWebsite">This website is open-source</a>
+              <a href="https://github.com/RaymondSalim/raymonds.dev">This website is open-source</a>
             </div>
             <div id="footer-credits">
               <span>Thanks to </span>
